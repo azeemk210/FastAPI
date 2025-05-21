eee3de1bbe6111d9d516210b068a430fdf174222
Mohammad Azeem Khan <khan.azeemak@gmail.com>
2025-05-21 16:24:35 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: float = None
 
 app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "This is your post"}
 
-@app.post("/create_posts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title: {payload['title']}, content: {payload['content']}"}
+@app.post("/createposts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    print(post_dict)
+    return {"data": post, "message": "Post created successfully"}
