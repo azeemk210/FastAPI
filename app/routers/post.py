a521212d406047511df7472d67260b94c8c56f12
Mohammad Azeem Khan <khan.azeemak@gmail.com>
2025-05-22 20:02:17 +0200

    added routers folder

---
+++ app/routers/post.py
@@ -0,0 +1,46 @@
+from .. import models, schemas	
+from fastapi import FastAPI, status, HTTPException, Depends, Response, APIRouter
+from ..database import get_db
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+@router.get("/posts", response_model=list[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+@router.get("/posts/{id}", response_model=schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
+
+@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first() == None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    post.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@router.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    db.refresh(post)
+    return post
